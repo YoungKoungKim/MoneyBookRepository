73afc8a34c115eec2e50312415cf768b629fbe5f
YoungKoungKim <ykk0413@gmail.com>
2017-04-19 20:02:04 +0900

    힘들다

---
+++ MoneyBookProject/src/controller/LogInterceptor.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class LogInterceptor extends HandlerInterceptorAdapter {
+	protected Log log = LogFactory.getLog(LogInterceptor.class);
+
+	@Override
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		log.info(" Request URI \t:  " + request.getRequestURI());
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		log.info(" Request URI \t:  " + request.getRequestURI());
+	}
+
+}
