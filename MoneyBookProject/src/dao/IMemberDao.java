24a7e9cdd4c7a6a6679af9c6a353a2159187b785
student <student@M90120>
2017-04-20 15:13:32 +0900

    매퍼,dao,서비스 수정

--- MoneyBookProject/src/dao/IMemberDao.java
+++ MoneyBookProject/src/dao/IMemberDao.java
@@ -1,16 +1,25 @@
 package dao;
 
-import java.util.List;
-
 import model.Member;
 
 public interface IMemberDao {
-   	public int insertMember(Member m);
-   	public int updateMember(String nick, String pwd, int id_index);			
-   	public int deleteMember(int id_index);		
-   	public List<Member> selectAllMember();			
-   	public Member selectOneMember(int id_index);
-   	public int selectIdIndex(String id);
-      public int selectNick(String nick);
+	// 회원가입
+	public int insertMember(Member member);
 
+	// 회원정보 수정
+	public int updateMember(Member member);
+
+	// 회원 삭제
+	public int deleteMember(int id_index);
+
+	// //전체 회원 리스트 이거 필요 없을 듯
+	// public List<Member> selectAllMember();
+	// id인덱스로 회원한명 가져오기
+	public Member selectOneMember(int id_index);
+
+	// 아이디로 인덱스 가져오기
+	public int selectIdIndex(String id);
+
+	// 닉네임으로 닉네임 검색해서 중복검사인 듯
+	public String selectNick(String nick);
 }
