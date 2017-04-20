4e3d754d8191a342b14ea098f03f9297af2a3aca
student <student@M90120>
2017-04-20 15:15:15 +0900

    컨트롤러

---
+++ MoneyBookProject/src/controller/MemberController.java
@@ -0,0 +1,99 @@
+package controller;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import model.Member;
+import service.IMemberService;
+
+// ****************************** viewResolver를 쓴다는 가정하에 했습니다.  *************************
+//******************************* model의 키값은 일단 대충 제가 맘대로 했습니다. *************************
+//******************************* 매개변수는 일단 기본값같은 설정은 하지 않았습니다. ************************
+
+@Controller
+public class MemberController {
+
+	@Autowired
+	private IMemberService memberService;
+
+	// join_Form
+	@RequestMapping("join_Form.do")
+	public String joinForm() {
+		return "joinForm";
+	}
+
+	// id_check
+	@RequestMapping("id_Check.do")
+	public @ResponseBody int idCheck(String id) {
+		return memberService.idCheck(id);
+	}
+
+	// nick_check
+	@RequestMapping("nick_Check.do")
+	public @ResponseBody int nickCheck(HttpServletResponse resp, String nick) {
+		return memberService.nickCheck(nick);
+	}
+
+	// join_Success
+	@RequestMapping(method = RequestMethod.POST, value = "join_Success.do")
+	public @ResponseBody int joinSuccess(Member m) {
+		return memberService.joinSuccess(m);
+	}
+
+	// login_Form
+	@RequestMapping("login_Form.do")
+	public String loginForm() {
+		return "loginForm";
+	}
+
+	// login_Success
+	@RequestMapping("login_Success.do")
+	public @ResponseBody int loginSuccess(HttpSession session, String id, String pwd, String beforePage) {
+		Member member = memberService.login(id, pwd);
+
+		if (member != null) {
+			session.setAttribute("id_index", member.getId_index());
+			return 2101;
+		} else {
+			return 2102;
+		}
+	}
+
+	@RequestMapping("logout.do")
+	public String logout(HttpSession session) {
+		session.removeAttribute("id_index");
+		// 로그아웃하면 메인으로??
+		return "redirect:main.do";
+	}
+
+	// password_Check
+	// 회원 정보 수정할 때 비번 확인인가??
+	@RequestMapping("password_Check.do")
+	public @ResponseBody int passwordCheck(int id_index, String pwd) {
+		return memberService.IdpwdCheck(pwd, id_index);
+	}
+
+	// inform_Update_Form
+	@RequestMapping("inform_Update_Form.do")
+	public ModelAndView informUpdateForm(Member member) {
+		ModelAndView mav = new ModelAndView();
+
+		mav.addObject("member", member);
+		mav.setViewName("informUpdateForm");
+
+		return mav;
+	}
+
+	// inform_Update
+	@RequestMapping("inform_Update.do")
+	public @ResponseBody int infromUpdate(Member member) {
+		return memberService.updateMember(member);
+	}
+}
