15905f040eac1c58a3ccfea787b4fa4277ad3e21
YoungKoungKim <ykk0413@gmail.com>
2017-04-20 14:43:32 +0900

    commit

--- MoneyBookProject/src/controller/MoneyBookController.java
+++ MoneyBookProject/src/controller/MoneyBookController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,21 +26,20 @@ public class MoneyBookController {
 	private IMoneyBookService moneyBookService;
 	@Autowired
 	private IBookMarkService bookMarkService;
-	
+
 	@Autowired
 	private IBoardService boardService;
-	
+
 	@RequestMapping("bookMarkDelete.do")
-	public 
-	@ResponseBody HashMap<String, Object> bookMarkDelete(int id_index, int bookmarkNo) {
+	public @ResponseBody HashMap<String, Object> bookMarkDelete(int id_index, int bookmarkNo) {
 		HashMap<String, Object> params = new HashMap<>();
 		params.put(BM.BOOKMARKNO, bookmarkNo);
 		params.put(BM.ID_INDEX, id_index);
 		int result = bookMarkService.bookMarkDelete(params);
-		
+
 		HashMap<String, Object> response = new HashMap<>();
 		if (result == 5101) {
-			//bookMark를 DB에서 삭제후 id_index와 date를 넘겨준다
+			// bookMark를 DB에서 삭제후 id_index와 date를 넘겨준다
 			response.put("result", true);
 			response.put("id_index", id_index);
 			response.put("msg", "북마크 삭제 성공");
@@ -48,18 +51,18 @@ public class MoneyBookController {
 		}
 		return response;
 	}
-	
+
 	@RequestMapping("bookMarkRegist.do")
-	public
-	@ResponseBody HashMap<String, Object> bookMarkRegist(int id_index, String category, String detail, int price) {
+	public @ResponseBody HashMap<String, Object> bookMarkRegist(int id_index, String category, String detail,
+			int price) {
 		int num = bookMarkService.checkbookNo(id_index);
 		HashMap<String, Object> response = new HashMap<>();
-		//bookMark개수가 6개 미만인지 확인
+		// bookMark개수가 6개 미만인지 확인
 		if (num >= 6) {
 			response.put("result", false);
 			response.put("msg", "북마크는 6개 이상을 등록할 수 없습니다.");
 		} else {
-			//6개 미만이라면 DB에 insert후 코드 리턴
+			// 6개 미만이라면 DB에 insert후 코드 리턴
 			HashMap<String, Object> params = new HashMap<>();
 			params.put(BM.ID_INDEX, id_index);
 			params.put(BM.CATEGORY, category);
@@ -67,11 +70,11 @@ public class MoneyBookController {
 			params.put(BM.PRICE, price);
 			int result = bookMarkService.bookMarkWrite(params);
 			if (result == 3101) {
-				//성공
+				// 성공
 				response.put("msg", "북마크 등록 성공");
 				response.put("result", true);
 			} else {
-				//실패
+				// 실패
 				response.put("msg", "북마크 등록 실패");
 				response.put("result", false);
 			}
@@ -79,23 +82,22 @@ public class MoneyBookController {
 		System.out.println(response.get("msg"));
 		return response;
 	}
-	
+
 	@RequestMapping("moneyBookUpdateForm.do")
 	public ModelAndView moneyBookUpdateForm(int id_index, Date date, int moneyBookNo) {
 		ModelAndView mav = new ModelAndView();
-		//클릭한 가계부 중 하나의 정보를 얻어옴 (selectOne)
+		// 클릭한 가계부 중 하나의 정보를 얻어옴 (selectOne)
 		mav.addObject("moneyBook", moneyBookService.moneyBookSelectOne(moneyBookNo, id_index, date));
-		//팝업창 uri
+		// 팝업창 uri
 		mav.setViewName("moneyBookUpdateForm");
 		return mav;
 	}
-	
+
 	@RequestMapping("moneyBookDelete.do")
-	public 
-	@ResponseBody HashMap<String, Object> moneyBookDelete(int id_index, int moneyBookNo) {
+	public @ResponseBody HashMap<String, Object> moneyBookDelete(int id_index, int moneyBookNo) {
 		HashMap<String, Object> response = new HashMap<>();
 		int result = moneyBookService.moneyBookDelete(moneyBookNo, id_index);
-		
+
 		if (result == 5001) {
 			response.put("msg", "상세내역 삭제 성공");
 			response.put("result", true);
@@ -106,13 +108,12 @@ public class MoneyBookController {
 		System.out.println(response.get("msg"));
 		return response;
 	}
-	
+
 	@RequestMapping("moneyBookUpdate.do")
-	public
-	@ResponseBody HashMap<String, Object> moneyBookUpdate(MoneyBook moneyBook) {
+	public @ResponseBody HashMap<String, Object> moneyBookUpdate(MoneyBook moneyBook) {
 		HashMap<String, Object> response = new HashMap<>();
 		int result = moneyBookService.moneyBookUpdate(moneyBook);
-		
+
 		if (result == 4201) {
 			response.put("msg", "내역 수정 성공");
 			response.put("result", true);
@@ -122,53 +123,58 @@ public class MoneyBookController {
 		}
 		return response;
 	}
-	
-	//혜리언니
+
+	// 혜리언니
 	@RequestMapping("viewMyPage.do")
-	public ModelAndView viewMyPage(int id_index, Date date){
+	public ModelAndView viewMyPage(int id_index, Date date) {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("monthContent" ,moneyBookService.getMonthContent(id_index, date));
-		mav.addObject("monthAmount" ,moneyBookService.totalMonthAmount(id_index, date));
-		mav.addObject("dayContent" ,moneyBookService.getDayContent(date, id_index));
-		mav.addObject("dayAmount" ,moneyBookService.totalMonthAmount(id_index, date));
+		mav.addObject("monthContent", moneyBookService.getMonthContent(id_index, date));
+		mav.addObject("monthAmount", moneyBookService.totalMonthAmount(id_index, date));
+		mav.addObject("dayContent", moneyBookService.getDayContent(date, id_index));
+		mav.addObject("dayAmount", moneyBookService.totalMonthAmount(id_index, date));
 		mav.setViewName("moneyBookView.jsp");
 		return mav;
 	}
-	
+
 	@RequestMapping("boardWriteForm.do")
-	public ModelAndView boardWriteForm(int id_index, Date date){
+	public ModelAndView boardWriteForm(int id_index, Date date) {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("monthContent" ,moneyBookService.getMonthContent(id_index, date));
-		mav.addObject("monthAmount" ,moneyBookService.totalMonthAmount(id_index, date));
+		mav.addObject("monthContent", moneyBookService.getMonthContent(id_index, date));
+		mav.addObject("monthAmount", moneyBookService.totalMonthAmount(id_index, date));
 		mav.addObject("date", moneyBookService.searchDate(date));
 		mav.setViewName("boardWrite.jsp");
 		return mav;
 	}
-	
-	
+
 	@RequestMapping("moneyBookWriteForm.do")
-	public ModelAndView moneyBookWriteForm(HashMap<String, Object> parmas){
+	public ModelAndView moneyBookWriteForm(HashMap<String, Object> parmas) {
 		ModelAndView mav = new ModelAndView();
-		//HashMap<String, Object> parmas = new HashMap<>();
+		// HashMap<String, Object> parmas = new HashMap<>();
 		mav.addAllObjects(bookMarkService.bookMarkSearch(parmas));
 		mav.setViewName("moneyBookAdd.jsp");
 		return mav;
 	}
-	
+
 	@RequestMapping("moneyBookDetailView.do")
-	public ModelAndView moneyBookDetailView(int id_index, Date date){
+	public ModelAndView moneyBookDetailView(int id_index, Date date) {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("dayContent" ,moneyBookService.getDayContent(date, id_index));
+		mav.addObject("dayContent", moneyBookService.getDayContent(date, id_index));
 		mav.setViewName("moneyBookView.jsp");
 		return mav;
 	}
-	
+
 	@RequestMapping("moneyBookRegist.do")
-	public ModelAndView  moneyBookRegist(MoneyBook mb){
+	public ModelAndView moneyBookRegist(MoneyBook mb) {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject(moneyBookService.moneyBookRegist(mb));
 		mav.setViewName("moneyBookView.jsp");
 		return mav;
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, true));
 	}
 
 }
