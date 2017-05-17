44b8dfd8176790025b89709281a085df51e075e7
kiuka <kiuka@DESKTOP-LQAN2V4>
2017-05-17 17:23:55 +0900

    test

--- MoneyBookProject/src/dao/IExtraBoardDao.java
+++ MoneyBookProject/src/dao/IExtraBoardDao.java
@@ -10,5 +10,5 @@ public interface IExtraBoardDao {
 	public int deleteExtraBoard(int boardNo);
 	public List<ExtraBoard> selectOne(int boardNo);
 	 
-	
+	public int deleteExtraBoardById_index(int id_index);
 }
